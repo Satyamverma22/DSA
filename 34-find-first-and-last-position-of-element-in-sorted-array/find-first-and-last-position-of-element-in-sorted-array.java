fcb157b004d4f3cfdc577c6ec8535c9b2514e8c0
Satyamverma22 <satyamverma3901@gmail.com>
2026-07-05 00:47:08 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (70.14%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,71 +1,54 @@
 class Solution {
+
+    public int lowerBound(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        int idx = n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] >= target) {
+                idx = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return idx;
+    }
+
+    public int upperBound(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        int idx = n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > target) {
+                idx = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return idx;
+    }
+
     public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
 
-        int[] ans = {-1, -1};
+        int lb = lowerBound(nums, target);
 
-        // Find first occurrence
-        ans[0] = firstOccurrence(nums, target);
-
-        // If target is not present, return {-1, -1}
-        if (ans[0] == -1) {
-            return ans;
+        if (lb == n || nums[lb] != target) {
+            return new int[]{-1, -1};
         }
 
-        // Find last occurrence
-        ans[1] = lastOccurrence(nums, target);
-
-        return ans;
-    }
-
-    // Function to find first occurrence
-    public int firstOccurrence(int[] nums, int target) {
-
-        int low = 0;
-        int high = nums.length - 1;
-        int ans = -1;
-
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;          // Store answer
-                high = mid - 1;     // Move left to find earlier occurrence
-            }
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-
-    // Function to find last occurrence
-    public int lastOccurrence(int[] nums, int target) {
-
-        int low = 0;
-        int high = nums.length - 1;
-        int ans = -1;
-
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;          // Store answer
-                low = mid + 1;      // Move right to find later occurrence
-            }
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
-        }
-
-        return ans;
+        return new int[]{lb, upperBound(nums, target) - 1};
     }
 }
