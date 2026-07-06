553b52ed845cb031f2fa511114212c44b834ddb8
Satyamverma22 <satyamverma3901@gmail.com>
2026-07-06 20:23:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
@@ -0,0 +1,27 @@
+class Solution {
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        int n = arr.length;
+        int repeating = -1;
+        int missing = -1;
+        
+        int[] freq = new int[n +1];
+        
+        for(int i = 0;i < n;i++){
+            freq[arr[i]]++;
+        }
+        for(int i = 1;i <= n;i++){
+            if(freq[i] == 2){
+                repeating = i;
+            }
+            if(freq[i] == 0){
+                missing = i;
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(repeating);
+        ans.add(missing);
+        
+        return ans;
+        
+    }
+}
