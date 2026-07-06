609d8e0a8e2429552db3ca159c3f5897111635db
Satyamverma22 <satyamverma3901@gmail.com>
2026-07-06 19:11:34 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (64.80%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean search(int[] nums, int target) {
+        int n = nums.length;
         int low = 0;
-        int high = nums.length - 1;
+        int high = n - 1;
 
         while (low <= high) {
             int mid = (low + high) / 2;
 
-            if (nums[mid] == target) {
-                return true;
-            }
+            if (nums[mid] == target) return true;
+
             if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
                 low++;
                 high--;
@@ -16,18 +16,21 @@ class Solution {
             else if (nums[low] <= nums[mid]) {
                 if (nums[low] <= target && target <= nums[mid]) {
                     high = mid - 1;
-                } else {
+                }
+                else {
                     low = mid + 1;
                 }
             }
             else {
                 if (nums[mid] <= target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
+                    low = mid + 1;   // Incorrect
+                }
+                else {
+                    high = mid - 1;    // Incorrect
                 }
             }
         }
-         return false;
+
+        return false;
     }
 }
