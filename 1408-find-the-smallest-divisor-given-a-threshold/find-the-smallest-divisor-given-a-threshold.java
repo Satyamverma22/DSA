3d4ea215b6e0b5b3dc1d1e598fefc50eb3fb0aae
Satyamverma22 <satyamverma3901@gmail.com>
2026-07-09 20:25:23 +0530

    Time: 28 ms (42.14%) | Memory: 51.1 MB (53.05%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,37 +1,40 @@
 class Solution {
-    public int sumByDiv(int[] nums, int div){
-        int sum = 0;
-        for(int i = 0; i < nums.length;i++){
-            sum += (int) Math.ceil((double) nums[i] / div);
-        }
-        return sum;
-    }
-
     public int findmax(int[] nums) {
-        int max = nums[0];
-
-        for (int num : nums) {
-            max = Math.max(max, num);
+        int max = 0;
+        for(int i = 0;i < nums.length;i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
         }
-
         return max;
     }
 
     public int smallestDivisor(int[] nums, int threshold) {
-        if(nums.length > threshold) return -1;
         int low = 1;
         int high = findmax(nums);
+        int ans = 1;
 
         while(low <= high){
-            int mid = (low + high)/2; 
+            int mid = (low + high)/2;
 
             if(sumByDiv(nums, mid) <= threshold){
                 high = mid - 1;
+                ans = mid; 
             }
             else{
                 low = mid + 1;
             }
         }
-        return low;
+        return ans;
     }
+    public int sumByDiv(int[] nums, int div){
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += (int) Math.ceil((double) nums[i] / div);
+        }
+
+        return sum;
+    }
+
 }
