8bf2890b0ca47c0ad0b067306535d40524f21382
Satyamverma22 <satyamverma3901@gmail.com>
2026-07-14 20:27:25 +0530

    Time: 674 ms (19.60%) | Memory: 42.9 MB (68.75%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,23 @@
+class Solution {
+    static boolean check(String s, int i, int j){
+        while(i <= j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            else{
+                j--;
+                i++;
+            }
+        }
+        return true;
+    }
+    public int countSubstrings(String s) {
+        int count = 0;
+        for(int i = 0;i < s.length();i++){
+            for(int j = i;j < s.length();j++){
+                if(check(s,i,j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
