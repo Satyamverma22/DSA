f071f67250df9b599c9910de63d3802fe98eb114
Satyamverma22 <satyamverma3901@gmail.com>
2026-07-27 21:55:59 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (92.16%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -3,10 +3,12 @@ class Solution {
         int n = nums.length;
         int low = 0;
         int high = n-1;
-        while(low <= high){
-            int mid = (low+high)/2;
+
+        while(low <=  high){
+            int mid = low + (high - low)/2;
+
             if(nums[mid] == target) return mid;
-            else if(nums[mid] > target) high = mid - 1;
+            if(nums[mid] > target) high = mid-1;
             else low = mid + 1;
         }
         return -1;
