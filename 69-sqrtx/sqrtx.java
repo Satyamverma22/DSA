cc7934c4b6739f699e50e8654ae5e3797a5bbe21
Satyamverma22 <satyamverma3901@gmail.com>
2026-08-03 00:12:52 +0530

    Time: 1 ms (97.52%) | Memory: 42.8 MB (8.51%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -13,11 +13,11 @@ class Solution {
             if(square == x){
                 return mid;
             }
-            else if(square > x) high = mid - 1;
-            else{
-                ans = mid;
+            else if(square < x){
+                 ans = mid;
                 low = mid + 1;
-            }
+            } 
+            else high = mid - 1;
         }
         return ans;
     }
